6ce08e1d587ac465c54cf3da5d7534ccc1b6bfd7
Ilyas11-03 <ilyas.aboulkassim@gmail.com>
2026-04-13 11:27:56 +0000

    issues of app/Init.py and interface/init.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,1 @@
-# Ce module contient l'interface utilisateur Streamlit
-# pour l'application d'analyse comportementale.
-# Ce fichier initialise le package interface et expose
-# les composants principaux de l'application web.
+"""Package d'interface Streamlit."""
